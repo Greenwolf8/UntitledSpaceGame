808934617fd69df9bd97de98d407473f75a4ee3b
Greenwolf8 <152461734+Greenwolf8@users.noreply.github.com>
2026-07-05 16:59:16 +1200

    fix: overhauled movement code
    Rewrote most of code because movement would not work when rolled or pitched

--- player2r.gd
+++ player2r.gd
@@ -22,45 +22,43 @@ func _unhandled_input(event: InputEvent) -> void:
 
 func _physics_process(delta: float) -> void:
 	var parent = get_parent() as RigidBody3D
+	if not parent:
+		return
+	
 	var input_dir := Input.get_vector ("move_left","move_right","move_forward", "move_back")
-	var local_dir := (transform.basis.x * input_dir.x + transform.basis.z * input_dir.y).normalized()
-	var gravity_dir = -parent.basis.y.normalized()
-	var direction:= Vector3.ZERO
+	var cam_basis := camera_3d2.global_transform.basis
+	var ship_basis : Basis = parent.global_transform.basis
+	var forward := -cam_basis.z
+	var right := cam_basis.x
 	
-	if camera and input_dir != Vector2.ZERO:
-		var cam_basis := camera.global_transform.basis
-		var forward := -cam_basis.z
-		var right := cam_basis.x
-		
-		forward.y = 0
-		forward = forward.normalized()
-		right.y = 0
-		right = right.normalized()
-		
+	forward = (forward - forward.project(ship_basis.y)).normalized()
+	right = (right - right.project(ship_basis.y)).normalized()
+	
+	var direction := Vector3.ZERO
+	if input_dir != Vector2.ZERO:
 		direction = (forward * -input_dir.y) + (right * input_dir.x)
-		
-		velocity.x = direction.x * speed
-		velocity.z = direction.z * speed
-
-	if is_on_floor():
-		velocity.y = 0
-	elif not is_on_floor():
-		velocity += gravity_dir * 11 * delta
-		
-	if local_dir:
-		velocity.x = direction.x * speed
-		velocity.z = direction.z * speed
 	
+	var gravity_dir = -ship_basis.y
+	
+	var current_horizontal_velocity = velocity - velocity.project(ship_basis.y)
+	var current_vertical_velocity = velocity.project(ship_basis.y)
+	
+	if direction != Vector3.ZERO:
+		current_horizontal_velocity = direction * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
-		velocity.z = move_toward(velocity.z, 0, speed)
-		
-	if Input.is_action_pressed("jump") and is_on_floor():
-			velocity.y = 4.5
-
+		current_horizontal_velocity = current_horizontal_velocity.move_toward(Vector3.ZERO, speed * delta * 10)
+	
+	if is_on_floor():
+		current_vertical_velocity = Vector3.ZERO
+		if Input.is_action_just_pressed("jump"):
+			current_vertical_velocity = ship_basis.y * 4.5
+	else:
+		current_vertical_velocity += gravity_dir * 11 * delta
+	
+	velocity = current_horizontal_velocity + current_vertical_velocity
+	
+	
 	move_and_slide()
-
-
 
 func enter_ship():
 	set_physics_process(false)
@@ -73,4 +71,3 @@ func leave_ship():
 	set_physics_process(true)
 	show()
 	camera_3d2.current = true 
-	
