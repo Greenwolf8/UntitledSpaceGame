3bd730d9f6ef4f15b480d7cf19db3d4e868cdae4
Greenwolf8 <152461734+Greenwolf8@users.noreply.github.com>
2026-07-09 22:09:01 +1200

    feat: Add cannon
    Can now fire projectiles out of the tip of the aircraft with left click or F.

--- Ship.gd
+++ Ship.gd
@@ -2,12 +2,19 @@ extends RigidBody3D
 
 @export var engine_power = 100.0
 @export var turn_torque = 30.0
+@export var bullet_scene : PackedScene = preload("res://Bullet.tscn")
+@onready var fire_point = %Hardpoint_1/Cannon/Cannon/MuzzleExit
+@onready var fire_timer = %Hardpoint_1/Cannon/Cannon/Fire_timer
 @onready var camera_3d: Camera3D = %Camera3D
 @onready var front_cast: RayCast3D = %FrontCast
 @onready var speed_label: Label = %Speed
 var Cameralocked = true
 var withPlayer = false
 
+func _ready() -> void:
+	print(fire_point)
+	print(fire_timer)
+	print(bullet_scene)
 
 func _unhandled_input(event: InputEvent) -> void:
 	if not Cameralocked and event is InputEventMouseMotion:
@@ -36,6 +43,9 @@ func _physics_process(_delta):
 	
 	speed_label.text = "Speed: " + str(current_speed)
 	
+	if Input.is_action_pressed("fire") and withPlayer:
+		shoot()
+	
 func interact_pressed():
 	var canEnter = not withPlayer
 	var hit_object = front_cast.get_collider()
@@ -57,6 +67,14 @@ func _input(_event: InputEvent) -> void:
 		%Camera3D.rotation_degrees.x = 0
 		%Camera3D.rotation_degrees.y = 90
 
+func shoot():
+	if fire_timer.is_stopped():
+		fire_timer.start()
+		var bullet = bullet_scene.instantiate()
+		get_tree().root.add_child(bullet)
+		bullet.global_transform = fire_point.global_transform
+	
+	
 func _enter_ship():
 	withPlayer = true
 	%OmniLight3D.light_color = Color(1.0, 0.945, 0.949, 1.0)
---
+++ bullet.gd
@@ -0,0 +1,7 @@
+extends CharacterBody3D
+
+@export var speed := 50
+
+func _physics_process(delta:):
+	var forward_direction = global_transform.basis.y * speed
+	global_position += forward_direction * speed * delta
